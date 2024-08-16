5ae1a2e501a2a7d32cf31c6030deba5c62272a50
felipe-pb <lfelipe.fis07@gmail.com>
2024-08-15 21:25:02 -0300

    implementação de entradas pelo usuario e texto de saida.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        int numero;
+        String agencia;
+        String nomeCliente;
+        Double saldo;
+        
+        System.out.println("Por favor, digite o número da Agência !");
+        numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite qual é a sua Agência !");
+        agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu nome !");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu saldo !");
+        saldo = scanner.nextDouble();
+
+        // USANDO O METODO "CONCAT" DA CLASSE STRING PARA CONCATENAR AS INFORMAÇÕES VINDAS DO USUARIO AO TEXTO, E TAMBEM O METODO "VALUEOF" PARA CONVERTER AS VARIAVEIS DO TIPO NUMERICO PARA STRING
+        System.out.println("Olá ".concat(nomeCliente).concat(", obrigado por criar uma conta em nosso banco, sua agência é ").concat(agencia).concat(", conta ").concat(String.valueOf(numero)).concat(" e seu saldo " ).concat(String.valueOf(saldo)).concat(" já está disponível para saque"));
+        
+        //FAZENDO CONCATENAÇÃO SIMPLES.
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo +" já está disponível para saque");
+
+        scanner.close();      
     }
 }
